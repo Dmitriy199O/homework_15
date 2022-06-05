e0cc767124d53b8aa79cd97fe9333ae45436a1ad
Дмитрий <mityai20031990@gmail.com>
2022-06-05 23:27:31 +0300

    almost_done

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask,jsonify
-import json
 import sqlite3
 from query import db_connect
 
@@ -9,10 +8,23 @@ app=Flask(__name__)
 def animal_info(item_id):
     query=f"""
          SELECT
-         animals_edited.id
-         ,animals_edited.age_upon_outcome
-         ,animals_edited.name
+              animals_edited.id
+              ,animals_edited.age_upon_outcome
+              ,animals_edited.name 
+              ,colour.colour 
+              ,breeds.breed 
+              ,types."type"
          FROM animals_edited
+         JOIN animals_colours
+            ON animals_colours.animal_id=animals_edited.id 
+         JOIN colour
+            ON colour.id=animals_colours.animal_id   
+         JOIN outcome  
+            ON outcome.id=animals_edited.outcome_id
+         JOIN types
+            ON types.id=animals_edited.type_id  
+         JOIN breeds
+            ON breeds.id=animals_edited.breed_id  
          WHERE animals_edited.id={item_id}
           """
     info = db_connect(query)
